5badd85c5bee3b2a8ba00c088517039517be6428
AurelieCh <98018100+AurelieCh@users.noreply.github.com>
2023-04-01 19:51:58 +0200

    {Ajout} FacturationService
    Ajout du service de facturation

---
+++ serveur-registre-facturation/src/main/java/fr/serveurregistrefacturation/services/FacturationService.java
@@ -0,0 +1,119 @@
+package fr.serveurregistrefacturation.services;
+
+import fr.serveurregistrefacturation.commun.Facturation;
+import fr.serveurregistrefacturation.exceptions.ExceptionBadRequest;
+import fr.serveurregistrefacturation.exceptions.ExceptionNotFound;
+import fr.serveurregistrefacturation.repositories.FacturationRepository;
+import fr.serveurregistrefacturation.services.dto.CreateFactureRequest;
+import fr.serveurregistrefacturation.services.dto.CreateFactureResponse;
+import fr.serveurregistrefacturation.services.dto.GetFactureResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class FacturationService {
+    @Autowired
+    private FacturationRepository facturationRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public CreateFactureResponse saveFacture(CreateFactureRequest factureRequest) throws Exception {
+        if(factureRequest.getAdresse().isBlank()
+                || factureRequest.getAdresse().isEmpty()
+                || factureRequest.getCommande().toString().isEmpty()
+                || factureRequest.getCommande().toString().isBlank()
+                || factureRequest.getEmail().isBlank()
+                || factureRequest.getEmail().isEmpty()) {
+            throw new ExceptionBadRequest("Les données en entrée du service sont non renseignés ou incorrectes. " +
+                    "L'une des données n'est pas présentes ou est incorrectes. Erreur 400");
+        }
+
+        //Premier compte fait référence au "localhost" -> Nom de l'appli.
+        ResponseEntity<String> response = restTemplate.getForEntity("http://comptes/comptes/checkMail?email=" + factureRequest.getEmail(), String.class);
+
+        if (!(response.getStatusCode() == HttpStatus.OK)) {
+            throw new ExceptionBadRequest("Les données en entrée du service sont non renseignés ou incorrectes. " +
+                    "Aucun compte trouvé avec cette email. Erreur 400");
+        } else {
+            Facturation toCreate;
+            toCreate = Facturation.builder()
+                    .adresse(factureRequest.getAdresse())
+                    .commande(factureRequest.getCommande())
+                    .dateCreation(new Date())
+                    .email(factureRequest.getEmail())
+                    .typePaiement(factureRequest.getTypePaiement())
+                    .build();
+
+            toCreate = this.facturationRepository.save(toCreate);
+
+            //Premier compte fait référence au "localhost" -> Nom de l'appli.
+            restTemplate.put("http://comptes/comptes/addFacture?id="+toCreate.getId()+"&email="+toCreate.getEmail(), String.class);
+            /**
+             * ResponseEntity<Void> envois =
+             * if (!(envois.getStatusCode() == HttpStatus.OK)) {
+             *                 throw new ExceptionBadRequest("Les données en entrée du service sont non renseignés ou incorrectes. " +
+             *                         "Erreur durant l'ajout de la facture au compte. Erreur 400");
+             *             }
+            */
+
+            return buildCreateFactureResponse(this.facturationRepository.save(toCreate));
+        }
+    }
+
+    /**
+     *
+     * @param f
+     * @return
+     *
+     * Fonction utilisée par saveFacture qui crée l'objet de réponse à retourner côté client.
+     *
+     */
+    private CreateFactureResponse buildCreateFactureResponse(Facturation f) {
+        return CreateFactureResponse.builder()
+                .email(f.getEmail())
+                .adresse(f.getAdresse())
+                .commande(f.getCommande())
+                .dateCreation(f.getDateCreation())
+                .typePaiement(f.getTypePaiement())
+                .build();
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     *
+     * Fonction utilisée par getFacture qui retourne les données d'une facture.
+     *
+     */
+    public GetFactureResponse getFacture(Integer id) throws ExceptionNotFound {
+        Optional<Facturation> temp = this.facturationRepository.findById(id);
+        if(temp.isEmpty()){
+            throw new ExceptionNotFound("Les données en entrée du service sont non renseignes ou incorrectes." +
+                    "Email inconnu. Erreur 204");
+        } else {
+            Facturation toReturn = temp.get();
+            return buildGetFactureResponse(toReturn);
+        }
+    }
+
+    private GetFactureResponse buildGetFactureResponse(Facturation f){
+        return GetFactureResponse.builder()
+                .id(f.getId())
+                .email(f.getEmail())
+                .adresse(f.getAdresse())
+                .commande(f.getCommande())
+                .dateCreation(f.getDateCreation())
+                .typePaiement(f.getTypePaiement())
+                .build();
+    }
+
+}
