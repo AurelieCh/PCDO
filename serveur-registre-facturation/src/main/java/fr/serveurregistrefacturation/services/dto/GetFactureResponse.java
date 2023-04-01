ad7836017e7f8d25f232d363e21fd13244292cf5
AurelieCh <98018100+AurelieCh@users.noreply.github.com>
2023-04-01 19:52:39 +0200

    {Ajout} GetFacture
    Ajout d'un dto permettant le retour d'une facture suite à une requête

---
+++ serveur-registre-facturation/src/main/java/fr/serveurregistrefacturation/services/dto/GetFactureResponse.java
@@ -0,0 +1,21 @@
+package fr.serveurregistrefacturation.services.dto;
+
+import fr.serveurregistrefacturation.commun.types.TypePaiement;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class GetFactureResponse {
+    private Long id;
+    private String email;
+    private String adresse;
+    private Date dateCreation;
+    private Integer commande;
+    private TypePaiement typePaiement;
+}
