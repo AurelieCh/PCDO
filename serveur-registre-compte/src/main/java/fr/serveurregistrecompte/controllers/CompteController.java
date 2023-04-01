a0bda207d9ac6aa86a0b78a33b566e91f9069bdf
AurelieCh <98018100+AurelieCh@users.noreply.github.com>
2023-04-01 19:29:32 +0200

    {Update} CompteController
    Modification du controller de compte, ajout de 3 nouvelles fonctions :
    La première permettant de supprimer un compte, la deuxième de vérifier qu'un compte existe (à partir d'un email) utile dans le projet facturations et la troisième pour ajouter une facture à la liste des factures du compte.

--- serveur-registre-compte/src/main/java/fr/serveurregistrecompte/controllers/CompteController.java
+++ serveur-registre-compte/src/main/java/fr/serveurregistrecompte/controllers/CompteController.java
@@ -1,21 +1,19 @@
 package fr.serveurregistrecompte.controllers;
 
-import fr.serveurregistrecompte.commun.exceptions.ErrorMessageClientException;
 import fr.serveurregistrecompte.commun.exceptions.ErrorMessageCompteException;
 import fr.serveurregistrecompte.commun.exceptions.ExceptionBadRequest;
 import fr.serveurregistrecompte.commun.exceptions.ExceptionNotFound;
 import fr.serveurregistrecompte.services.CompteService;
 import fr.serveurregistrecompte.services.dtoCompte.CreateCompteRequest;
+import fr.serveurregistrecompte.services.dtoCompte.DeleteCompteRequest;
+import fr.serveurregistrecompte.services.dtoCompte.GetCompteRequest;
 import fr.serveurregistrecompte.services.dtoCompte.ModifyCompteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
-import java.util.List;
-
 @RestController
-@RequestMapping("comptes")
+@RequestMapping
 /**
  * Le controller des comptes, c'est lui qui va communiquer avec notre base de données de compte
  * (supposément dans le cas ou il y a une base pour les comptes clients à part) et le client.
@@ -36,11 +34,10 @@ public class CompteController {
         }
     }
 
-    @GetMapping
-    public ResponseEntity getComptes(@RequestParam(value = "email", required = false) String email,
-                                     @RequestParam(value = "password", required = false) String password) throws Exception {
+    @GetMapping("getCompte")
+    public ResponseEntity getComptes(@RequestBody GetCompteRequest getCompteRequest) throws Exception {
         try {
-            return ResponseEntity.ok().body(this.compteService.GetCompte(email, password));
+            return ResponseEntity.ok().body(this.compteService.GetCompte(getCompteRequest));
         } catch (ExceptionNotFound e){
             return ResponseEntity.noContent().build();
         } catch (ExceptionBadRequest e){
@@ -53,13 +50,50 @@ public class CompteController {
     @PutMapping
     public ResponseEntity updateCompte(@RequestBody ModifyCompteRequest compteRequest) throws Exception{
         try{
-            return ResponseEntity.ok().body(this.compteService.updateClient(compteRequest));
+            return ResponseEntity.ok().body(this.compteService.updateCompte(compteRequest));
         } catch (ExceptionBadRequest e){
-            return ResponseEntity.badRequest().body(new ErrorMessageClientException(e.getMessage()));
+            return ResponseEntity.badRequest().body(new ErrorMessageCompteException(e.getMessage()));
         } catch (Exception e){
-            return ResponseEntity.internalServerError().body(new ErrorMessageClientException(e.getMessage()));
+            return ResponseEntity.internalServerError().body(new ErrorMessageCompteException(e.getMessage()));
         }
 
+    }
+
+    @DeleteMapping()
+    public ResponseEntity delCompte(@RequestBody DeleteCompteRequest deleteCompteRequest) throws Exception{
+        try{
+            return ResponseEntity.ok().body(this.compteService.delCompte(deleteCompteRequest));
+        } catch (ExceptionBadRequest e){
+            return ResponseEntity.badRequest().body(new ErrorMessageCompteException(e.getMessage()));
+        } catch (ExceptionNotFound e){
+            return ResponseEntity.noContent().build();
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(new ErrorMessageCompteException(e.getMessage()));
+        }
+    }
+
+    @PutMapping("addFacture")
+    public ResponseEntity addFacture(@RequestParam(value = "id", required = true) Integer id,
+                                     @RequestParam(value = "email", required = true) String email) throws Exception{
+        try{
+            return ResponseEntity.ok().body(this.compteService.AddFacture(id, email));
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(new ErrorMessageCompteException(e.getMessage()));
+        }
+
+    }
+
+    @GetMapping("checkMail")
+    public ResponseEntity verifyCompte(@RequestParam(value = "email", required = true) String email) throws Exception{
+        try{
+            return ResponseEntity.ok().body(this.compteService.verifyCompte(email));
+        } catch (ExceptionBadRequest e) {
+            return ResponseEntity.badRequest().body(new ErrorMessageCompteException(e.getMessage()));
+        } catch (ExceptionNotFound e){
+            return ResponseEntity.noContent().build();
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(new ErrorMessageCompteException(e.getMessage()));
+        }
     }
 
 }
