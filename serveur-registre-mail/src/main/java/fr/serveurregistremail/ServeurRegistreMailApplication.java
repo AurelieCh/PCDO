850361bf7abb97cd202240d6c6afb7ae59a6fc83
AurelieCh <98018100+AurelieCh@users.noreply.github.com>
2023-04-03 14:34:19 +0200

    {Ajout} Service de mail
    Ajout des fichiers principaux nécessaires à l'ajout d'un service de mail

---
+++ serveur-registre-mail/src/main/java/fr/serveurregistremail/ServeurRegistreMailApplication.java
@@ -0,0 +1,13 @@
+package fr.serveurregistremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurRegistreMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurRegistreMailApplication.class, args);
+	}
+
+}
