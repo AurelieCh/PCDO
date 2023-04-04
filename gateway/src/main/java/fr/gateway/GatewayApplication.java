d940abaa5f59136a58e754508e0fcef96d32a200
Johan Marliere <66920436+operfield@users.noreply.github.com>
2023-04-04 17:09:29 +0200

    oauth retiré a springboot pour des test

--- gateway/src/main/java/fr/gateway/GatewayApplication.java
+++ gateway/src/main/java/fr/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
-@EnableWebFluxSecurity
+
 public class GatewayApplication {
 
 	public static void main(String[] args) {
