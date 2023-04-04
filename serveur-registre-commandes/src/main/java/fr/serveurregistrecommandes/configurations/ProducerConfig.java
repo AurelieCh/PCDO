468ddaf8d2138c0243cebce01bcd95ae89e4054a
AurelieCh <98018100+AurelieCh@users.noreply.github.com>
2023-04-04 10:49:47 +0200

    {Update} Debug serialization
    Ajout de fonctions permettant le debug de la serialization

--- serveur-registre-commandes/src/main/java/fr/serveurregistrecommandes/configurations/ProducerConfig.java
+++ serveur-registre-commandes/src/main/java/fr/serveurregistrecommandes/configurations/ProducerConfig.java
@@ -1,6 +1,11 @@
 package fr.serveurregistrecommandes.configurations;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +15,17 @@ public class ProducerConfig {
     @Bean
     public DirectExchange direct() {
         return new DirectExchange("exchange.direct");
+    }
+
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 }
--- serveur-registre-mail/src/main/java/fr/serveurregistremail/configurations/ConsumerConfig.java
+++ serveur-registre-mail/src/main/java/fr/serveurregistremail/configurations/ConsumerConfig.java
@@ -1,10 +1,10 @@
 package fr.serveurregistremail.configurations;
 
-import org.springframework.amqp.core.AnonymousQueue;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +27,18 @@ public class ConsumerConfig {
         return BindingBuilder.bind(autoDeleteQueue)
                 .to(direct)
                 .with("key.1");
+    }
+
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 }
 
